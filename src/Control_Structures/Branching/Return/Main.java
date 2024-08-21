8d14c75f49e8b3f8240ab140ab4320669defe7d3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 11:58:08 +0530

    The return statement

---
+++ src/Control_Structures/Branching/Return/Main.java
@@ -0,0 +1,16 @@
+package Control_Structures.Branching.Return;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(sum(10, 20));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b; // Exit method and return sum
+    }
+}
+
+/**
+ * The return statement is used to exit a method and optionally return a value to the caller.
+ */
