d35fc5868aa9470cb4f64f7586316df9ca8c32f0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 11:50:29 +0530

    for Loop

---
+++ src/Control_Structures/Looping/For/For.java
@@ -0,0 +1,22 @@
+package Control_Structures.Looping.For;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+/**
+
+ for (initialization; condition; increment/decrement) {
+ // Code to be executed repeatedly
+ }
+
+ * Initialization: This step is executed once before the loop starts.
+ * Condition: The loop continues as long as this condition is true.
+ * Increment/Decrement: This step is executed after each iteration.
+
+ */
