54d2f65c9975e9ad9033e7058f7f14c9696a73ed
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 11:53:32 +0530

    Enhanced for Loop (For-Each Loop)

---
+++ src/Control_Structures/Looping/For_Each/ForEach.java
@@ -0,0 +1,20 @@
+package Control_Structures.Looping.For_Each;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+    }
+}
+
+/**
+
+ for (dataType element : arrayOrCollection) {
+ // Code to be executed for each element
+ }
+
+ */
