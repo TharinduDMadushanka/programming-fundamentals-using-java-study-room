a82d23f3e49a47fa923083ef1e21b861bcd7571f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 11:51:23 +0530

    while Loop

---
+++ src/Control_Structures/Looping/While/While.java
@@ -0,0 +1,20 @@
+package Control_Structures.Looping.While;
+
+public class While {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+}
+
+/**
+
+ while (condition) {
+ // Code to be executed repeatedly
+ }
+
+ */
