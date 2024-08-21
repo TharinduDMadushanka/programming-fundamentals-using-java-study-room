4aef676ea9905e1713dbf9e7f47aa38293564780
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 11:38:04 +0530

    Control Structure & if and else

---
+++ src/Control_Structures/Conditional/If/If.java
@@ -0,0 +1,21 @@
+package Control_Structures.Conditional.If;
+
+public class If {
+    public static void main(String[] args) {
+
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        }else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
+
+/**
+
+ if (condition) {
+ // Code to be executed if condition is true
+ }
+
+ */
