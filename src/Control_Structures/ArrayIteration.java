7d3f8f87b690a60de92429521dad0e304180a6f4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-21 12:00:06 +0530

    Example of Iterating Through an Array Using a for-each Loop

---
+++ src/Control_Structures/ArrayIteration.java
@@ -0,0 +1,11 @@
+package Control_Structures;
+
+public class ArrayIteration {
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Green", "Blue"};
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+}
